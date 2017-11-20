3d1d20c379251c88e12de4a995e5336550a438eb
harshabakku <bakkubaku@gmail.com>
2017-11-20 18:54:31 +0530

    remove some unused code, and a file

--- changellyTest.js
+++ changellyTest.js
@@ -20,30 +20,6 @@ var web3 = new Web3();
   var contract;
   var contractAddress;
 
-function sign(web3, address, value, callback) {
-  web3.eth.sign(address, value, function(err, sig) {
-    if (!err) {
-      try {
-        var r = sig.slice(0, 66);
-        var s = '0x' + sig.slice(66, 130);
-        var v = parseInt('0x' + sig.slice(130, 132), 16);
-        if (sig.length<132) {
-          //web3.eth.sign shouldn't return a signature of length<132, but if it does...
-          sig = sig.slice(2);
-          r = '0x' + sig.slice(0, 64);
-          s = '0x00' + sig.slice(64, 126);
-          v = parseInt('0x' + sig.slice(126, 128), 16);
-        }
-        if (v!=27 && v!=28) v+=27;
-        callback(undefined, {r: r, s: s, v: v});
-      } catch (err) {
-        callback(err, undefined);
-      }
-    } else {
-      callback(err, undefined);
-    }
-  });
-}
 
 //calls contract with exchanged ether
   function BuyTokensUsingEther (etherReceived, contract, etherWalletAddr){
